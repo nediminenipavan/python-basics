125b9658e7b4e130cc700c7d7bfc2b620e8cd466
nediminenipavan-tech <nediminenipavan@gmail.com>
2026-01-06 15:17:53 +0530

    jenkins

---
+++ variables.py
@@ -0,0 +1,62 @@
+num_servers = 10 # this is an inline comment
+# this a comment 
+a = 10 
+
+
+# mutli line string 
+"""
+This is a
+sample 
+comment
+"""
+
+
+# Normal string 
+"Hello world"
+'Hello world'
+'today\'s weather is nice'
+"today's weather is nice"
+
+# Data types 
+"""
+1. Number (integer, Float)
+2. string 
+3. Boolean
+4. list
+5. Tuple
+6. Dict (Dictonories, Hashmaps)
+7. sets
+"""
+num_servers = 10 # integer
+print(num_servers)
+Const_pi = 3.14 # Float
+print(type(Const_pi))
+
+num_servers = 15
+print(num_servers)
+print(type(num_servers))
+
+
+server = "ansible" # string
+print(type(server))
+
+is_prod = False # boolean 
+print(type(is_prod))
+
+
+a = 10
+b = 20
+total = a + b # 30 
+diff = a - b # -10
+multi = a * b # 200
+division = b / a # 2.0
+print(division) 
+integer_division = b // a 
+print(integer_division)
+remainder = b % a # 0
+print(remainder)
+a = 2
+b = 3 
+power = b ** a  # 9 
+print(power)
+
